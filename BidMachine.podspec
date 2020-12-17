1f7ad8ffdb144382de9652d237bd75c490c20b6f
AssassinSC <ilia.log.1990@gmail.com>
2020-12-17 22:51:23 +0300

    release 1.6.4

--- BidMachine.podspec
+++ BidMachine.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "BidMachine"
-  s.version      = "1.6.3"
+  s.version      = "1.6.4"
   s.summary      = "BidMachine iOS SDK"
 
   s.description  = <<-DESC
--- BidMachine/BidMachine/BDMDefines.m
+++ BidMachine/BidMachine/BDMDefines.m
@@ -8,7 +8,7 @@
 
 #import "BDMDefines.h"
 
-NSString * const kBDMVersion = @"1.6.3";
+NSString * const kBDMVersion = @"1.6.4";
 
 NSString * const kBDMUserGenderMale     = @"M";
 NSString * const kBDMUserGenderFemale   = @"F";
